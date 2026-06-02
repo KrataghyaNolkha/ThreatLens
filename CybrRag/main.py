eb5c8a30f521ad1119a416a6238fb06209a6201e
Krataghya <krataghyanolakha34@gmail.com>
2026-06-02 12:27:53 +0530

    fix: update CORS to allow frontend requests from Vercel

--- CybrRag/main.py
+++ CybrRag/main.py
@@ -156,8 +156,8 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://127.0.0.1:3000", "http://127.0.0.1:3001", "http://127.0.0.1:3002"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
